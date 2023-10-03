6484661355c67ce1c875ece4ddb1d7739663f833
k-ishida <60775539+ki412412@users.noreply.github.com>
2023-10-04 01:07:01 +0900

    refactor: Remove writable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
 	import { writable } from "svelte/store";
 
-    const a = writable(3); // secret(private key)
-    const g = writable(3);
-    const q = writable(200);
+    let a = 3; // secret(private key)
+    let g = 3;
+    let q = 200;
 
-    const k = writable(2);
-    const p = writable(5);
-    const h = writable(0);
-    const c = writable(3);
-    const s = writable(0);
+    let k = 2;
+    let p = 5;
+    let h = 0;
+    let c = 3;
+    let s = 0;
 
     $: {
-        $h = ($g**$k) % $p;
-        $s = (($a * $c) + $k) % $q;
+        h = (g**k) % p;
+        s = ((a * c) + k) % q;
     }
 </script>
 
@@ -21,8 +21,8 @@
 
 <div class="mt-6">
     <div>
-        g: <input type="number" bind:value={$g}>
-        q: <input type="number" bind:value={$q}>
+        g: <input type="number" bind:value={g}>
+        q: <input type="number" bind:value={q}>
     </div>
 </div>
 
@@ -33,7 +33,7 @@
         <h2 class="text-lg text-center">Alice</h2>
 
         <div class="h-10 mt-3">
-            a(secret, private key): <input type="number" bind:value={$a}>
+            a(secret, private key): <input type="number" bind:value={a}>
         </div>
 
         <!-- flows -->
@@ -41,13 +41,13 @@
             <!-- Step 0 -->
             <div class="h-24">
                 <div class="font-semibold">Step 0: Alice calculates Public Key(PK) and send it to Bob</div>
-                <div class="mt-3 pl-6">PK = g^a <span class="text-gray-400"> = {$g}^{$a}</span> = {$g ** $a}</div>
+                <div class="mt-3 pl-6">PK = g^a <span class="text-gray-400"> = {g}^{a}</span> = {g ** a}</div>
             </div>
             <!-- Step 1 -->
             <div class="mt-6 h-24">
-                <div class="font-semibold">Step 1: Alice picks a random k in range 1,...,q({$q})</div>
+                <div class="font-semibold">Step 1: Alice picks a random k in range 1,...,q({q})</div>
                 <div class="mt-3 pl-6">
-                    k: <input type="number" bind:value={$k} min="0" max={$q}>
+                    k: <input type="number" bind:value={k} min="0" max={q}>
                 </div>
             </div>
             <!-- Step 2 -->
@@ -55,14 +55,14 @@
                 <div class="font-semibold">Step 2: Alice calculates h and send it to Bob</div>
                 <div class="mt-3 pl-6">
                     <div>h = g^k mod p</div>
-                    <div class="text-gray-400">{$h} = {$g}^{$k} mod {$p}</div>
+                    <div class="text-gray-400">{h} = {g}^{k} mod {p}</div>
                 </div>
             </div>
             <!-- Step 3 -->
             <div class="mt-6 h-24">
                 <div class="font-semibold">Step 3: Alice recieves c from Bob</div>
                 <div class="mt-3 pl-6">
-                    c = {$c}
+                    c = {c}
                 </div>
             </div>
             <!-- Step 4 -->
@@ -70,7 +70,7 @@
                 <div class="font-semibold">Step 4: Alice calculates s and send it to Bob</div>
                 <div class="mt-3 pl-6">
                     <div>s = ac + k mod q</div>
-                    <div class="text-gray-400">{$s} = {$a}*{$c} + {$k} mod {$p}</div>
+                    <div class="text-gray-400">{s} = {a}*{c} + {k} mod {p}</div>
                 </div>
             </div>
             <!-- Step 5 -->
@@ -91,26 +91,26 @@
             <!-- Step 0 -->
             <div class="h-24">
                 <div class="font-semibold">Step 0: Bob recieves PK from Alice</div>
-                <div class="mt-3 pl-6">PK = {$g**$a}</div>
+                <div class="mt-3 pl-6">PK = {g**a}</div>
             </div>
             <!-- Step 1 -->
             <div class="mt-6 h-24"></div>
             <!-- Step 2 -->
             <div class="mt-6 h-24">
                 <div class="font-semibold">Step 2: Bob recieves h from Alice</div>
-                <div class="mt-3 pl-6">h = {$h}</div>
+                <div class="mt-3 pl-6">h = {h}</div>
             </div>
             <!-- Step 3 -->
             <div class="mt-6 h-24">
-                <div class="font-semibold">Step 3: Bob picks a random c in range 1,...,q({$q}) and send it to Alice</div>
+                <div class="font-semibold">Step 3: Bob picks a random c in range 1,...,q({q}) and send it to Alice</div>
                 <div class="mt-3 pl-6">
-                    c: <input type="number" bind:value={$c} min="0" max={$q}>
+                    c: <input type="number" bind:value={c} min="0" max={q}>
                 </div>
             </div>
             <!-- Step 4 -->
             <div class="mt-6 h-24">
                 <div class="font-semibold">Step 4: Bob recieves s from Alice</div>
-                <div class="mt-3 pl-6">s = {$s}</div>
+                <div class="mt-3 pl-6">s = {s}</div>
             </div>
             <!-- Step 5 -->
             <div class="mt-6 h-30">
@@ -118,9 +118,9 @@
                 <div class="mt-3 pl-6">
                     <div>g^s ≡ (PK)^c * h mod p</div>
                     <div class="text-gray-400">g^s ≡ (g^a)^c * h mod p</div>
-                    <div class="text-gray-400">{$g}^{$s}≡({$g}^{$a})^{$c} * {$h} mod {$p}</div>
-                    <div class="text-gray-400">{($g**$s)} ≡ {((($g**$a)**$c) * $h)} mod {$p}</div>
-                    <div class="text-gray-400">{($g**$s) % $p} = {((($g**$a)**$c) * $h) % $p}</div>
+                    <div class="text-gray-400">{g}^{s}≡({g}^{a})^{c} * {h} mod {p}</div>
+                    <div class="text-gray-400">{(g**s)} ≡ {(((g**a)**c) * h)} mod {p}</div>
+                    <div class="text-gray-400">{(g**s) % p} = {(((g**a)**c) * h) % p}</div>
                 </div>
             </div>
         </div>
